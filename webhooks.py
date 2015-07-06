1ed4d71795b41b2f83e65ad1671e65061cea255c
Kevin Price <kprice@yahoo-inc.com>
2015-07-06 12:28:58 -0500

    Guarantee removal of temporal payload file
    Guarantee that the temporal payload file is removed even if an
    exception occurs during storage of the temporary payload file
    or during execution of the hook scripts.

--- webhooks.py
+++ webhooks.py
@@ -145,30 +145,33 @@ def index():
     with open(tmpfile, 'w') as pf:
         pf.write(dumps(payload))
 
-    # Run scripts
+    # Store return information for each script run
     ran = {}
-    for s in scripts:
 
-        proc = Popen(
-            [s, tmpfile, event],
-            stdout=PIPE, stderr=PIPE
-        )
-        stdout, stderr = proc.communicate()
+    try:
+        # Run scripts
+        for s in scripts:
 
-        ran[basename(s)] = {
-            'returncode': proc.returncode,
-            'stdout': stdout,
-            'stderr': stderr,
-        }
+            proc = Popen(
+                [s, tmpfile, event],
+                stdout=PIPE, stderr=PIPE
+            )
+            stdout, stderr = proc.communicate()
 
-        # Log errors if a hook failed
-        if proc.returncode != 0:
-            logging.error('{} : {} \n{}'.format(
-                s, proc.returncode, stderr
+            ran[basename(s)] = {
+                'returncode': proc.returncode,
+                'stdout': stdout,
+                'stderr': stderr,
+            }
+
+            # Log errors if a hook failed
+            if proc.returncode != 0:
+                logging.error('{} : {} \n{}'.format(
+                    s, proc.returncode, stderr
             ))
-
-    # Remove temporal file
-    remove(tmpfile)
+    finally:
+        # Remove temporal file
+        remove(tmpfile)
 
     info = config.get('return_scripts_info', False)
     if not info:
