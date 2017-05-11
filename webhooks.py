c1351fcdeec1b955ab9278a686506d11af069a75
Carlos Jenkins <carlos@jenkins.co.cr>
2017-05-11 15:13:16 -0700

    Fixed missing quotes in hmac digestmod

--- webhooks.py
+++ webhooks.py
@@ -79,7 +79,7 @@ def index():
             abort(501)
 
         # HMAC requires the key to be bytes, but data is string
-        mac = hmac.new(str(secret), msg=request.data, digestmod="sha1")
+        mac = hmac.new(str(secret), msg=request.data, digestmod='sha1')
 
         # Python prior to 2.7.7 does not have hmac.compare_digest
         if hexversion >= 0x020707F0:
