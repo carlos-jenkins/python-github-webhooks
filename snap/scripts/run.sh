dcf80c2558289d85034076320100a902b7f71c48
Marco Trevisan (Treviño) <mail@3v1n0.net>
2018-10-20 21:57:18 +0200

    snap: add snap packaging support via snapcraft
    Hooks needs to be installed in $SNAP_USER_DATA/hooks

---
+++ snap/scripts/run.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ ! -f "$SNAP_USER_COMMON/config.json" ]; then
+    cp "$SNAP/config.json.sample" "$SNAP_USER_COMMON/config.json"
+fi
+
+if [ ! -L "$SNAP_USER_COMMON/webhooks.py" ]; then
+    ln -sf "$SNAP/webhooks.py" "$SNAP_USER_COMMON"
+fi
+
+if [ "X$SNAP_ARCH" = "Xamd64" ]; then
+  ARCH="x86_64-linux-gnu"
+elif [ "X$SNAP_ARCH" = "Xarmhf" ]; then
+  ARCH="arm-linux-gnueabihf"
+elif [ "X$SNAP_ARCH" = "Xarm64" ]; then
+  ARCH="aarch64-linux-gnu"
+else
+  ARCH="$SNAP_ARCH-linux-gnu"
+fi
+
+export PATH=$SNAP/usr/bin:$PATH
+export LD_LIBRARY_PATH=$SNAP/usr/lib/$ARCH:$SNAP/usr/lib:$LD_LIBRARY_PATH
+
+cd "$SNAP_USER_COMMON"
+mkdir -p hooks
+
+exec env $SNAP/usr/bin/python3 ./webhooks.py $@
