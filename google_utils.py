b8b5b80a8481473ca91b38ef72ec20123442b6f1
tomas.fernandez <tomas.fernandez@leroymerlin.es>
2020-03-18 17:56:00 +0100

    Removing useless step in Google Auth

--- google_utils.py
+++ google_utils.py
@@ -1,12 +1,10 @@
-import google
 from google.oauth2 import service_account
-from google.auth.transport.requests import AuthorizedSession
+import google.auth.transport.requests
 
     #DOC: https://firebase.google.com/docs/database/rest/auth
 def getGoogleAccessToken(service_account_info):
     scopes = ['https://www.googleapis.com/auth/cloud-platform']
     credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=scopes)
-    AuthorizedSession(credentials)
     request = google.auth.transport.requests.Request()
     credentials.refresh(request)
     return credentials.token
